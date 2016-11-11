52e8c24046201ce351601a74e7d805cd0c1c2f50
betterelephant <betterelephant@gmail.com>
2016-11-11 22:41:12 +0000

    var app

--- public/js/map.js
+++ public/js/map.js
@@ -1,5 +1,5 @@
-angular.module('contactsApp', ['leaflet-directive']);
-controller("MarkerController", [ '$scope', function($scope) {
+var app = angular.module('contactsApp', ['leaflet-directive']);
+app.controller("MarkerController", [ '$scope', function($scope) {
     angular.extend($scope, {
         osloCenter: {
             lat: 59.91,

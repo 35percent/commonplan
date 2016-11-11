801f20e33d2856d86fa4ad968941b3aeeaa6ad79
betterelephant <betterelephant@gmail.com>
2016-11-11 22:32:34 +0000

    angular leaflet

---
+++ public/js/map.js
@@ -0,0 +1,22 @@
+angular.module('contactsApp', ['leaflet-directive']);
+app.controller("MarkerController", [ '$scope', function($scope) {
+    angular.extend($scope, {
+        osloCenter: {
+            lat: 59.91,
+            lng: 10.75,
+            zoom: 12
+        },
+        markers: {
+            osloMarker: {
+                lat: 59.91,
+                lng: 10.75,
+                message: "I want to travel here!",
+                focus: true,
+                draggable: false
+            }
+        },
+        defaults: {
+            scrollWheelZoom: false
+        }
+    });
+}]);

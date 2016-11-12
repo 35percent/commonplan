15a287737f9f853ad46b382e0100dd44fc524344
betterelephant <betterelephant@gmail.com>
2016-11-12 18:34:12 +0000

    contact.lat

--- public/js/app.js
+++ public/js/app.js
@@ -124,7 +124,7 @@ var mainMarker = {
                 markers: {
                     mainMarker: angular.copy(mainMarker)
                 },
-                position: {
+                contact: {
                     lat: 51,
                     lng: 0
                 },
@@ -136,43 +136,10 @@ var mainMarker = {
                 }
             });
             $scope.$on("leafletDirectiveMarker.dragend", function(event, args){
-                $scope.position.lat = args.model.lat;
-                $scope.position.lng = args.model.lng;
+                $scope.contact.lat = args.model.lat;
+                $scope.contact.lng = args.model.lng;
             });
     })
-    .controller('MarkersSimpleController', [ '$scope', function($scope) {
-            var mainMarker = {
-                lat: 51,
-                lng: 0,
-                focus: true,
-                message: "Hey, drag me if you want",
-                draggable: true
-            };
-            angular.extend($scope, {
-                london: {
-                    lat: 51.505,
-                    lng: -0.09,
-                    zoom: 8
-                },
-                markers: {
-                    mainMarker: angular.copy(mainMarker)
-                },
-                position: {
-                    lat: 51,
-                    lng: 0
-                },
-                events: { // or just {} //all events
-                    markers:{
-                      enable: [ 'dragend' ]
-                      //logic: 'emit'
-                    }
-                }
-            });
-            $scope.$on("leafletDirectiveMarker.dragend", function(event, args){
-                $scope.position.lat = args.model.lat;
-                $scope.position.lng = args.model.lng;
-            });
-        } ])
     .controller("EditContactController", function($scope, $routeParams, Contacts) {
         Contacts.getContact($routeParams.contactId).then(function(doc) {
             $scope.contact = doc.data;

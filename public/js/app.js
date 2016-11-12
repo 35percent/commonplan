edf9e6a2552f3f2001d674e5e93f618278479433
betterelephant <betterelephant@gmail.com>
2016-11-12 15:40:10 +0000

    put map in the contacts form

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ angular.module("contactsApp", ['ngRoute', 'leaflet-directive'])
                 }
             })
             .when("/new/contact", {
-                controller: "NewContactController",
+                controller: "MarkersSimpleController",
                 templateUrl: "contact-form.html"
             })
             .when("/contact/:contactId", {
@@ -109,6 +109,39 @@ angular.module("contactsApp", ['ngRoute', 'leaflet-directive'])
             });
         }
     })
+    .controller('MarkersSimpleController', [ '$scope', function($scope) {
+            var mainMarker = {
+                lat: 51,
+                lng: 0,
+                focus: true,
+                message: "Hey, drag me if you want",
+                draggable: true
+            };
+            angular.extend($scope, {
+                london: {
+                    lat: 51.505,
+                    lng: -0.09,
+                    zoom: 8
+                },
+                markers: {
+                    mainMarker: angular.copy(mainMarker)
+                },
+                position: {
+                    lat: 51,
+                    lng: 0
+                },
+                events: { // or just {} //all events
+                    markers:{
+                      enable: [ 'dragend' ]
+                      //logic: 'emit'
+                    }
+                }
+            });
+            $scope.$on("leafletDirectiveMarker.dragend", function(event, args){
+                $scope.position.lat = args.model.lat;
+                $scope.position.lng = args.model.lng;
+            });
+        } ])
     .controller("EditContactController", function($scope, $routeParams, Contacts) {
         Contacts.getContact($routeParams.contactId).then(function(doc) {
             $scope.contact = doc.data;

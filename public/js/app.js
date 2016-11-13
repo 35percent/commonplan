d105753f44696fc713dd22f0aea88be0b22432a6
betterelephant <betterelephant@gmail.com>
2016-11-13 15:18:32 +0000

    contact singular

--- public/js/app.js
+++ public/js/app.js
@@ -72,7 +72,7 @@ angular.module("contactsApp", ['ngRoute', 'leaflet-directive'])
     })
     .controller("ListController", function(contacts, $scope) {
         $scope.contacts = contacts.data;
-        var latd = $scope.contacts.phone.work
+        var latd = $scope.contact.phone.work
             angular.extend($scope, {
                 london: {
                     lat: 51.505,

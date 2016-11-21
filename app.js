352374aea22c09f78d23a7521b09a96ab58e48f3
betterelephant <betterelephant@gmail.com>
2016-11-21 17:46:04 +0000

    controller.js

--- app.js
+++
@@ -1,8 +0,0 @@
-angular.module("justMap.config", [])
-.constant("name", "51.5044")
-.constant("description", "A MEAN app integrated with leaflet that allows users to add comments to a map.")
-.constant("repository", "https://github.com/35percent/commonplan")
-.constant("logo", "https://pbs.twimg.com/profile_images/481778845620518912/fIPiDX4c.jpeg")
-.constant("keywords", ["node","express","angular","MEAN","mongodb","REST","API"])
-.constant("addons", ["mongolab"])
-.constant("env", {"LATITUDE":{"description":"The latitude coordinate for the centre of the map.","value":"51.05"},"LONGITUDE":{"description":"The longitude coordinate for the centre of the map.","value":"-0.09"}});
--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,5 @@ var gulpNgConfig = require('gulp-ng-config');
 gulp.task('configvars', function () {
   gulp.src('app.json')
   .pipe(gulpNgConfig('justMap.config'))
-  .pipe(gulp.dest('.'))
+  .pipe(gulp.dest('public/js'))
 });

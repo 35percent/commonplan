43d3b53c3cf365a50ce9bb2d8fa52fe9f262c09b
betterelephant <betterelephant@gmail.com>
2016-11-21 16:23:14 +0000

    configvars

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var gulpNgConfig = require('gulp-ng-config');
 
-gulp.task('test', function () {
+gulp.task('configvars', function () {
   gulp.src('app.json')
   .pipe(gulpNgConfig('justMap.config'))
   .pipe(gulp.dest('.'))

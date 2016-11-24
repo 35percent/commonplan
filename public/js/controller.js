f1341f738394ddc99ce1b86d119e3d86483f4810
betterelephant <betterelephant@gmail.com>
2016-11-24 19:36:52 +0000

    return

--- public/js/controller.js
+++ public/js/controller.js
@@ -101,7 +101,7 @@ angular.module("contactsApp", ['ngRoute', 'leaflet-directive'])
 
 angular.extend($scope, {
                 center: {
-                    lat: latitude(), 
+                    lat: return latitude, 
                     lng: -0.1008511,
                     zoom: 15
                 },
